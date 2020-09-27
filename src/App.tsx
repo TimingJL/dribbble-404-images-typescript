cb3012b81d4d074daf6cda6b269f77e1d1861d90
taiming <taiming@bridgewell.com>
2020-09-27 09:08:17 +0800

    [feature] add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import styled from 'styled-components/macro';
+import Navigation from 'containers/Navigation';
+import MainPage from 'containers/MainPage';
+import Footer from 'containers/Footer';
 
-function App() {
+const FullScreenContainer = styled.div`
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+`;
+
+const App = () => {
 	return (
-		<>
-			<nav>nav</nav>
-			<main>main</main>
-			<footer>footer</footer>
-		</>
+		<FullScreenContainer>
+			<Navigation />
+			<MainPage />
+			<Footer />
+		</FullScreenContainer>
 	);
-}
+};
 
 export default App;
---
+++ src/containers/MainPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+const Container = styled.main`
+	flex: 1 1 auto;
+`;
+
+const MainPage = () => {
+	return <Container>Main page</Container>;
+};
+
+export default MainPage;
