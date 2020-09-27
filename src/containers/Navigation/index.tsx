02755f4ed3421ae0554f7e56595b714b67e2280d
taiming <taiming@bridgewell.com>
2020-09-27 20:09:12 +0800

    [feature] styling navigation

--- src/containers/Navigation/index.tsx
+++ src/containers/Navigation/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import styled from 'styled-components/macro';
+
+const Nav = styled.nav`
+	display: flex;
+	justify-content: space-between;
+	padding: 32px;
+`;
+
+const Banner = styled.a`
+	font-family: 'Pacifico', cursive;
+	font-size: 26px;
+	text-decoration: none;
+	color: #bebebe;
+`;
 
 const Navigation = () => {
-	return <nav>Navigation</nav>;
+	return (
+		<Nav>
+			<Banner href={'https://www.cakeresume.com/taiming'} target="_blank">
+				Taiming
+			</Banner>
+		</Nav>
+	);
 };
 
 export default Navigation;
